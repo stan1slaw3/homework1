19ea0ecef2912ac5590a9840d3a2816c8e7b02b2
stan1slaw3 <rakovskiy509@gmail.com>
2024-02-23 08:30:22 +0100

    hw1 js

---
+++ homework/hw1.js
@@ -0,0 +1,43 @@
+{
+var a= 'hello';
+    console.log(a)
+var a = 'owu';
+    console.log(a)
+    var a = 'com';
+    console.log(a)
+    var a = 'ua';
+    console.log(a)
+    var a = 1;
+    console.log(a)
+    var a = 10;
+    console.log(a)
+    var a = -999;
+    console.log(a)
+    var a = 123;
+    console.log(a)
+    var a = 3.14;
+    console.log(a)
+    var a = 2.7;
+    console.log(a)
+    var a = 16;
+    console.log(a)
+    var a = true;
+    console.log(a)
+    var a = false;
+    console.log(a)
+}
+{
+    var firstname = 'Stanislav ';
+    var middlename = 'Rakovskiy ';
+    var lastname = 'Serhiyovich';
+    var result = firstname.concat(middlename,lastname);
+    console.log(result)
+}
+{
+    let a = 100;
+    console.log(typeof a)
+    let b = '100'
+    console.log(typeof b)
+    let c = true;
+    console.log(typeof c)
+}
